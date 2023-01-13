be5d88f489b8895ac24a6c6139a68dec3f301fe7
skywalk1411 <todoskywalk@gmail.com>
2023-01-13 16:41:55 -0500

    working version

--- router.js
+++ router.js
@@ -10,24 +10,24 @@ const jimp = require('jimp');
 //const image = require('images');
 const CommentRoutes = express.Router();
 CommentRoutes.use(formidable({
-    maxFileSize: 5*1024*1024,
-    maxTotalFileSize: 5*1024*1024,
+    maxFileSize: 5 * 1024 * 1024,
+    maxTotalFileSize: 5 * 1024 * 1024,
     encoding: 'utf-8',
-    uploadDir: path.join(__dirname, 'public','upload'),
+    uploadDir: path.join(__dirname, 'public', 'upload'),
     multiples: true,
     keepExtensions: true
 }));
 // const PNG = require("pngjs").PNG;
 // https://github.com/oliver-moran/jimp#image-manipulation-methods-default-plugins
-const resizeThumb = async (image,object)=> {
+const resizeThumb = async (image, object) => {
     const payload = await jimp.read(image);
-    payload.resize(100,jimp.AUTO);
-    await payload.writeAsync(`${path.join(object.dir,object.name+`.png`)}`);
+    payload.resize(100, jimp.AUTO);
+    await payload.writeAsync(`${path.join(object.dir, object.name + `.png`)}`);
     return true;
 };
 CommentRoutes.post('/', async function (req, res) {
-    console.log('file=> size: ',req.files.image.size,' path: ',req.files.image.path, ' name: ',req.files.image.name, ' type: ',req.files.image.type, ' hash: ',req.files.image.hash, ' modAt: ',req.files.image.lastModifiedDate);
-    console.log('fields=>',req.fields)
+    console.log('file=> size: ', req.files.image.size, ' path: ', req.files.image.path, ' name: ', req.files.image.name, ' type: ', req.files.image.type, ' hash: ', req.files.image.hash, ' modAt: ', req.files.image.lastModifiedDate);
+    console.log('fields=>', req.fields)
     if (req.fields?.title && req.fields?.message) {
         //has title and message
         if (req.files?.image?.size !== 0 && req.files?.image?.name !== '') {
@@ -35,18 +35,29 @@ CommentRoutes.post('/', async function (req, res) {
             //has file image
             const imageFile = path.parse(req.files.image.path);
             console.log(imageFile)
-            console.log('image=> root: ',imageFile.root, ' dir: ',imageFile.dir, ' base: ',imageFile.base, ' ext: ', imageFile.ext, ' name: ',imageFile.name);
-            const finalPath = `${path.join(imageFile.dir, imageFile.name + '.png')}`;
-            console.log(finalPath)
+            console.log('image=> root: ', imageFile.root, ' dir: ', imageFile.dir, ' base: ', imageFile.base, ' ext: ', imageFile.ext, ' name: ', imageFile.name);
+            const loadPath = `${path.join(imageFile.dir, imageFile.base)}`;
+            const finalPath = `${path.join(imageFile.dir, imageFile.name+'.png')}`;
+            console.log(loadPath, ' ', finalPath, typeof(loadPath), typeof(finalPath))
             try {
-                const transform = await resizeThumb(finalPath,imageFile);
+
+                await jimp.read(loadPath)
+                    .then(image => {
+                        return image
+                            .resize(100, jimp.AUTO)
+                            .quality(80)
+                            .writeAsync(finalPath)
+                    })
+                    .catch(err => {
+                        throw err;
+                    })
+                /*const transform = await resizeThumb(finalPath,imageFile);
                 switch (transform) {
                     case true:
                         console.log('ok');
                         break;
                     case false:
-                        console.log('nay');
-                }
+                        console.log('nay');*/
                 /*await jimp.read(imageFile)
                     .then(image=> {
                         return image
@@ -61,7 +72,6 @@ CommentRoutes.post('/', async function (req, res) {
                 .size(100)
                 .encode("jpg")
                 .save(finalPath, { quality: 80})*/
-
                 /*sharp(req.files.image.path)
                 .resize({
                     fit: sharp.fit.contain,
@@ -73,10 +83,20 @@ CommentRoutes.post('/', async function (req, res) {
             } catch (err) {
                 if (err) {
                     console.log(err)
-                }
-                else {
-                    console.log('else')
-                    
+                    try {
+                        await jimp.read(loadPath)
+                            .then(image => {
+                                return image
+                                    .resize(100, jimp.AUTO)
+                                    .quality(80)
+                                    .writeAsync()
+                            })
+                            .catch(err => {
+                                throw err;
+                            })
+                    } catch (err) {
+                        if (err) throw err
+                    }
                 }
             }
             fs.unlink(req.files.image.path, (err) => {
@@ -84,9 +104,7 @@ CommentRoutes.post('/', async function (req, res) {
             })
             console.log(req.files.image.filepath, req.files.image.mimetype, req.files.image.hash);
             console.log(req.files.image.name, req.files.image.size);
-            
             res.render('home/home', { received: { title: req.fields?.title, message: req.fields?.message }, image: `/upload/${req.files.image.name}` })
-            
         }
         else {
             //error no image
