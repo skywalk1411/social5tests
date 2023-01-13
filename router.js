093292a5bd35cdda472ccba264e090da2c78af09
skywalk1411 <todoskywalk@gmail.com>
2023-01-13 17:55:57 -0500

    line 36 crash, line 35 always false

--- router.js
+++ router.js
@@ -32,11 +32,26 @@ CommentRoutes.post('/', async function (req, res) {
         if (req.files?.image?.size !== 0 && req.files?.image?.name !== '') {
             //has file image
             const imageFile = path.parse(req.files.image.path);
-            console.log('file=> size: ', req.files.image.size, ' path: ', req.files.image.path, ' name: ', req.files.image.name, ' type: ', req.files.image.type, ' hash: ', req.files.image.hash, ' modAt: ', req.files.image.lastModifiedDate);
-            console.log('fields=>', req.fields)
-            console.log('image=> root: ', imageFile.root, ' dir: ', imageFile.dir, ' base: ', imageFile.base, ' ext: ', imageFile.ext, ' name: ', imageFile.name);
+            //const finalExt = (req.files.image.type === '.gif') ? '.gif' : '.png';
+            const finalExt = (req.files.image.type === 'image/gif') ? '.gif' : '.png';
             const loadPath = `${path.join(imageFile.dir, imageFile.base)}`;
-            const finalPath = `${path.join(imageFile.dir, imageFile.name+'.png')}`;
+            const finalPath = `${path.join(imageFile.dir, imageFile.name+`${finalExt}`)}`;
+            console.log('file=> size: ', req.files.image.size, ' path: ', req.files.image.path, ' name: ', req.files.image.name, ' type: ', req.files.image.type, ' hash: ', req.files.image.hash, ' modAt: ', req.files.image.lastModifiedDate);
+            console.log('------------------------------')
+            console.log('fields=>', req.fields)
+            console.log('------------------------------')
+            console.log('image=> root: ', imageFile.root, ' dir: ', imageFile.dir, ' base: ', imageFile.base, ' ext: ', imageFile.ext, ' name: ', imageFile.name);
+            console.log('------------------------------')
+            let testlength = `${req.files.image.type.length}`
+            console.log(testlength.length, typeof(testlength), typeof(req.files.image.type), testlength, req.files.image.type)
+            console.log('------------------------------')
+            console.assert(req.files.image.type === 'image/gif','image not ===image/gif');
+            console.assert(req.files.image.type == 'image/gif','image not ==image/gif');
+            console.assert(req.files.image.type === '.gif','image not .gif', req.files.image.type);
+            console.assert(req.files.image.type === '.jpg','image not .jpg', req.files.image.type);
+            console.assert(req.files.image.type === '.png','image not .png', req.files.image.type);
+            console.log('------------------------------')
+            console.log(loadPath, ' ', finalPath, ' ', finalExt, req.files.image.type)
             try {
                 await jimp.read(loadPath)
                     .then(image => {
@@ -67,10 +82,11 @@ CommentRoutes.post('/', async function (req, res) {
                     }
                 }
             }
+            
             fs.unlink(req.files.image.path, (err) => {
                 if (err) throw err;
             })
-            res.render('home/home', { received: { title: req.fields?.title, message: req.fields?.message }, imagez: `/upload/${imageFile.name}.png` })
+            res.render('home/home', { received: { title: req.fields?.title, message: req.fields?.message }, imagez: `/upload/${imageFile.name}${finalExt}` })
         }
         else {
             //error no image
